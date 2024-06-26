a80c2e41f86baa5727d75d52cea673e056bef34a
romiyadangol <dangolromiya@gmail.com>
2024-06-26 23:03:00 +0545

    Added Server

---
+++ Server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.post('/generate-password', (req,res) => {
+    const payload = req.body;
+    const {length, isNumber, isUppercase, isLowercase, isSpecialChar} = payload;
+
+    const number = '0123456789';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const specialCharacter = '!@#$%^&*()_+';
+
+    let characters ='';
+    if (isNumber) {
+        characters += number;
+    }
+    if (isUppercase) {
+        characters += uppercase;
+    }
+    if (isLowercase) {
+        characters += lowercase;
+    }
+    if (isSpecialChar) {
+        characters += specialCharacter;
+    }
+    
+
+    let password = '';
+    for(let i=0;i<length;i++){
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    res.json({password});
+
+})
+app.listen(port,()=> console.log('Server is running on port '+port));
