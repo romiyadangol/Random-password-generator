3fdbdbb4dc09919cc1e563984b26f195559fdf0c
romiyadangol <dangolromiya@gmail.com>
2024-06-27 15:25:35 +0545

    Added Client

---
+++ Client/src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import axios from 'axios';
+
+function App() {
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(50);
+  const [isUppercase, setUppercase] = useState(true);
+  const [isLowercase, setLowercase] = useState(true);
+  const [isNumber, setNumber] = useState(true);
+  const [isSpecialChar, setSpecialCharacter] = useState(true);
+
+  const generatePassword = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/generate-password', {
+        length,
+        isNumber,
+        isUppercase,
+        isLowercase,
+        isSpecialChar,
+      });
+      console.log('Password generated:', response.data.password);
+      setPassword(response.data.password);
+    } catch (error) {
+      console.error('Error generating password:', error);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="header-wrapper">
+          <h1>Random Password Generator</h1>
+          <p>Create strong and secure passwords to keep your account safe online.</p>
+        </div>
+
+        <div className="input-wrapper">
+          <button onClick={generatePassword}>Regenerate</button>
+          {password && (
+            <div>
+              <h2>Password:</h2>
+              <input type="text" value={password} readOnly />
+            </div>
+          )}
+        </div>
+
+        <div className="password-generator">
+          <label htmlFor="length">Password Length:</label>
+          <input
+            type="number"
+            id="length"
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+          />
+        </div>
+
+        <div className="characters-used">
+          <label>Characters Used:</label>
+
+          <div>
+            <input
+              type="checkbox"
+              id="uppercase"
+              checked={isUppercase}
+              onChange={(e) => setUppercase(e.target.checked)}
+            />
+            <label htmlFor="uppercase">ABC</label>
+          </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="lowercase"
+              checked={isLowercase}
+              onChange={(e) => setLowercase(e.target.checked)}
+            />
+            <label htmlFor="lowercase">abc</label>
+          </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="number"
+              checked={isNumber}
+              onChange={(e) => setNumber(e.target.checked)}
+            />
+            <label htmlFor="number">123</label>
+          </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="specialCharacter"
+              checked={isSpecialChar}
+              onChange={(e) => setSpecialCharacter(e.target.checked)}
+            />
+            <label htmlFor="specialCharacter">#$%</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/generate-password', (req,res) => {
     const payload = req.body;
